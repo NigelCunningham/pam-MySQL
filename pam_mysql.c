a9f75d286719293b91895d7b1e798ebe07570aaf
jwsdv <juergen.weiss@sdv-it.de>
2017-07-07 16:21:06 +0200

    Bugfixed memory bug in Base64Encode (for SSHA encoding)

--- pam_mysql.c
+++ pam_mysql.c
@@ -771,19 +771,22 @@ static size_t calcDecodeLength(const char* b64input) {
 int Base64Encode(const unsigned char* buffer, size_t length, char** b64text) {
 	BIO *bio, *b64;
 	BUF_MEM *bufferPtr;
+	size_t b64len;
 
 	b64 = BIO_new(BIO_f_base64());
 	bio = BIO_new(BIO_s_mem());
 	bio = BIO_push(b64, bio);
 
 	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Ignore newlines - write everything in one line
+	BIO_set_close(bio, BIO_CLOSE);
 	BIO_write(bio, buffer, length);
 	BIO_flush(bio);
-	BIO_get_mem_ptr(bio, &bufferPtr);
-	BIO_set_close(bio, BIO_NOCLOSE);
-	BIO_free_all(bio);
 
-	*b64text=(*bufferPtr).data;
+	b64len = bufferPtr->length;
+	(*b64text) = (char *) malloc((b64len + 1) * sizeof(char));
+	memcpy(*b64text, bufferPtr->data, b64len);
+	(*b64text)[b64len] = '\0';
+	BIO_free_all(bio);
 
 	return (0); //success
 }
