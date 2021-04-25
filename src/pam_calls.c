3b37be4d5d2fbb9f139bd435ac60f93ea4975e83
Nigel Cunningham <nigel@nigelcunningham.com.au>
2021-04-25 21:22:36 +1200

    Fix recursive call to self in pam_calls.c
    Signed-off-by: Nigel Cunningham <nigel@nigelcunningham.com.au>

--- src/pam_calls.c
+++ src/pam_calls.c
@@ -6,5 +6,5 @@ int pam_mysql_get_user(pam_handle_t *pamh, const char **user, const char *prompt
 }
 
 int pam_mysql_get_item(const pam_handle_t *pamh, int item_type, const void **item) {
-  return pam_mysql_get_item(pamh, item_type, item);
+  return pam_get_item(pamh, item_type, item);
 }
