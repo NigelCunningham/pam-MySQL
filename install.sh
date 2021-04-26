3de969a32afbe8ed5c80b3cce2e799ce654b98ab
Nigel Cunningham <nigel@nigelcunningham.com.au>
2021-04-26 22:19:00 +1200

    Handle Redhat based directory structure in install script.
    Signed-off-by: Nigel Cunningham <nigel@nigelcunningham.com.au>

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-mv -f /lib/security/libpam_mysql.so /lib/security/pam_mysql.so
-strip /lib/security/pam_mysql.so
+strip /lib/security/libpam_mysql.so
+if [ -f /etc/redhat-release ]; then
+  mv -f /lib/security/libpam_mysql.so /usr/lib64/security/pam_mysql.so
+  rmdir /lib/security
+else
+  mv -f /lib/security/libpam_mysql.so /lib/security/pam_mysql.so
+fi
