ebdd166af297fe5af8e3d6a9ab4fbf2c5d629cf5
Anton Avramov <lukav@lukav.com>
2021-08-23 08:08:48 -0400

    Add test for bcrypt blowfish

--- src/password.c
+++ src/password.c
@@ -11,6 +11,7 @@ extern pam_mysql_err_t pam_mysql_encrypt_password_joomla15(pam_mysql_ctx_t *ctx,
 extern pam_mysql_err_t pam_mysql_encrypt_password_ssha(pam_mysql_ctx_t *ctx, const char *unencrypted, char *encrypted);
 extern pam_mysql_err_t pam_mysql_encrypt_password_sha512(pam_mysql_ctx_t *ctx, const char *unencrypted, char *encrypted);
 extern pam_mysql_err_t pam_mysql_encrypt_password_sha256(pam_mysql_ctx_t *ctx, const char *unencrypted, char *encrypted);
+extern pam_mysql_err_t pam_mysql_encrypt_password_blowfish(pam_mysql_ctx_t *ctx, const char *unencrypted, char *encrypted);
 
 static void pam_mysql_free_encrypted_xallocd(char *buf) {
   char *p = buf - 1;
@@ -29,6 +30,7 @@ pam_mysql_password_encryption_t pam_mysql_password_plugins[] = {
   {7, "ssha", 41, pam_mysql_encrypt_password_ssha, pam_mysql_free_encrypted_xallocd, "TZgPw6HJaiWzaqkXgAyyyh2IoGhjNGVm" },
   {8, "sha512", 128, pam_mysql_encrypt_password_sha512, pam_mysql_free_encrypted_xallocd, "ee5f868a7d5194434d0a70c8464439848fc5f8b0719f3929877e05b62053edd6fca02171ce561da335632d6e3e59f1b47059cd2b83e940702eab3a3f4305185f" },
   {9, "sha256", 64, pam_mysql_encrypt_password_sha256, pam_mysql_free_encrypted_xallocd, "c6e8cefec7fea272e327c452c74cf7f6335193efb28dfb7063cf22b998fc6f9a" },
+  {10, "blowfish", 60, pam_mysql_encrypt_password_crypt, pam_mysql_free_encrypted_xallocd, "$2y$10$onYkUEKoKBcWnrgFqjq5.eTPF1oRE.WZ9bG6u9MqgUVDeukGzyqIy" },
 };
 
 long unsigned int pam_mysql_num_plugins(void)
